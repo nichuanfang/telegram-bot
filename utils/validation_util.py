39e55828e7829089d69881579f1ce3087273808f
f18326186224 <f18326186224@gmail.com>
2024-05-27 01:10:43 +0800

    feat: dogyun_bot调试完毕

---
+++ utils/validation_util.py
@@ -0,0 +1,17 @@
+import os
+
+from utils.my_logging import get_logger
+
+logger = get_logger('validation_util')
+
+
+# 校验环境变量
+def validate(*keys):
+	values = []
+	for keyword in keys:
+		env = os.environ.get(keyword)
+		if env is None or env == '':
+			logger.error(f'{keyword}未设置!')
+			raise ValueError(f'{keyword}未设置!')
+		values.append(env)
+	return values
