f2f1bb411520123aaf76a6efc066d5c48d1dcabc
f18326186224 <f18326186224@gmail.com>
2024-05-27 17:38:52 +0800

    feat: 定时任务调试完毕

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
+import asyncio
 import multiprocessing
 import os
 
+import aiocron
 import dotenv
-from telegram.ext import ApplicationBuilder
-
-from utils import my_logging, bot_util
 
 dotenv.load_dotenv(override=True)
+from telegram import Bot
+from telegram.ext import ApplicationBuilder
+
+from bots.dogyun_bot.scheduled_task import lucky_draw_notice, balance_lack_notice
+
+from utils import my_logging, bot_util
 
 logger = my_logging.get_logger('app')
 
@@ -14,9 +19,24 @@ logger = my_logging.get_logger('app')
 bot_directories = [d for d in os.listdir('bots') if os.path.isdir(os.path.join('bots', d))]
 
 
+async def add_scheduled_tasks(bot):
+	@aiocron.crontab('0 0 7 * *')  # 每月7号
+	async def get_traffic_packet():
+		await get_traffic_packet(bot)
+	
+	@aiocron.crontab('0 9 * * *')  # 每天9点
+	async def lucky_draw_task():
+		await lucky_draw_notice(bot)
+	
+	@aiocron.crontab('0 9 * * *')  # 每天9点
+	async def balance_lack_task():
+		await balance_lack_notice(bot)
+
+
 def start_bot(bot_name, token, command_handlers=None):
 	if token is None:
 		logger.error("请先设置BOT TOKEN!")
+		return
 	
 	application = ApplicationBuilder() \
 		.token(token) \
@@ -33,28 +53,42 @@ def start_bot(bot_name, token, command_handlers=None):
 	application.run_polling(drop_pending_updates=True)
 
 
+async def start_scheduler(token):
+	bot = Bot(token=token)
+	await add_scheduled_tasks(bot)
+	logger.info("Scheduler started.")
+	try:
+		while True:
+			await asyncio.sleep(1)
+	except (KeyboardInterrupt, SystemExit):
+		pass
+
+
+def run_scheduler(token):
+	asyncio.run(start_scheduler(token))
+
+
 processes = []
 # 动态加载每个机器人
 for bot_directory in bot_directories:
 	try:
-		# 动态导入每个机器人的处理器 同时指明了需要导入的属性(函数,子模块等)
 		bot_module = __import__(f'bots.{bot_directory}.bot', fromlist=['handlers'])
-		
 		handlers = getattr(bot_module, 'handlers')
 		
-		# todo 如果模块是dogyun 则还要添加定时任务
-		if bot_directory == 'dogyun_bot':
-			pass
-		
 		token = os.getenv(f'{bot_directory.upper()}_TOKEN')
-		if token is None:
+		if token is None or len(token) == 0:
 			logger.error(f'{bot_directory.upper()}_TOKEN未设置!')
 		
+		if bot_directory == 'dogyun_bot':
+			# 创建一个单独的进程来运行调度器
+			p_scheduler = multiprocessing.Process(target=run_scheduler, args=(token,))
+			processes.append(p_scheduler)
+		
 		command_handlers = handlers()
-		p = multiprocessing.Process(target=start_bot, args=(bot_directory, token, command_handlers))
-		processes.append(p)
+		p_bot = multiprocessing.Process(target=start_bot, args=(bot_directory, token, command_handlers))
+		processes.append(p_bot)
 	except ImportError as e:
-		print(f"Failed to import bot {bot_directory}: {e}")
+		logger.error(f"Failed to import bot {bot_directory}: {e}")
 
 if __name__ == '__main__':
 	for p in processes:
