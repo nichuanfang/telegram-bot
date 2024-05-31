4a9eb4917d61d081377cfcfac5763a69b89d0c7f
f18326186224 <f18326186224@gmail.com>
2024-06-01 02:57:47 +0800

    fix: markdown解析错误

--- my_utils/bot_util.py
+++ my_utils/bot_util.py
@@ -1,4 +1,5 @@
 import asyncio
+import re
 
 from telegram.constants import ChatAction
 
@@ -16,3 +17,11 @@ async def send_typing_action(update):
 		await update.message.reply_chat_action(action=ChatAction.TYPING)
 		# 每隔4秒发送一次“正在输入...”状态
 		await asyncio.sleep(4)
+
+
+def escape_markdown_v2(text: str) -> str:
+	"""
+	Escape special characters for Telegram MarkdownV2.
+	"""
+	escape_chars = r'_*[]()~`>#+-=|{}.!'
+	return re.sub(f'([{re.escape(escape_chars)}])', r'\\\1', text)
