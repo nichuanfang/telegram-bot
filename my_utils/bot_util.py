563ed50317562b7a73145ecfc3bdf1519a28a013
f18326186224 <f18326186224@gmail.com>
2024-06-12 21:57:08 +0800

    feat: 转义失败发送原消息

--- my_utils/bot_util.py
+++ my_utils/bot_util.py
@@ -1,9 +1,8 @@
 import asyncio
 import re
-import time
 
 from telegram import Update
-from telegram.constants import ChatAction
+from telegram.constants import ParseMode
 from telegram.ext import CallbackContext
 
 
@@ -21,11 +20,27 @@ async def send_typing_action(update: Update, context: CallbackContext, flag_key)
 		await asyncio.sleep(2)  # 每2秒发送一次 typing 状态
 
 
+async def send_message(update: Update, text):
+	try:
+		escaped_text = escape_markdown_v2(text)  # 转义特殊字符
+		await update.message.reply_text(escaped_text,
+		                                reply_to_message_id=update.message.message_id,
+		                                parse_mode=ParseMode.MARKDOWN_V2)
+	except:
+		await update.message.reply_text(text, reply_to_message_id=update.message.message_id)
+
+
 def escape_markdown_v2(text: str) -> str:
 	"""
 	Escape special characters for Telegram MarkdownV2 and replace every pair of consecutive asterisks (**) with a single asterisk (*).
 	"""
 	try:
-		return re.sub(f"([{re.escape(r'_[]()~>#+-=|{}.!')}])", r'\\\1', text.replace('**', '*'))
+		# List of characters that need to be escaped in MarkdownV2
+		escape_chars = r'_[]()~>#+-=|{}.!'
+		# Escape each character
+		escaped_text = re.sub(f"([{re.escape(escape_chars)}])", r'\\\1', text)
+		# Replace every pair of consecutive asterisks (**) with a single asterisk (*)
+		escaped_text = escaped_text.replace('**', '*')
+		return escaped_text
 	except Exception as e:
-		return e
+		return str(e)
