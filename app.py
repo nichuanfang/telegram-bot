0c4ea1a96e55e697b321b04762988be0a55f2f91
f18326186224 <f18326186224@gmail.com>
2024-06-14 10:27:35 +0800

    feat: telegram启动参数调整

--- app.py
+++ app.py
@@ -49,10 +49,13 @@ def start_bot(bot_name, token, command_handlers=None):
 		return
 	application = ApplicationBuilder() \
 		.token(token) \
+		.read_timeout(15) \
+		.write_timeout(15) \
+		.connect_timeout(10) \
 		.concurrent_updates(True) \
 		.get_updates_read_timeout(60) \
 		.get_updates_write_timeout(60) \
-		.get_updates_connect_timeout(60) \
+		.get_updates_connect_timeout(10) \
 		.build()
 	
 	application.add_handlers(command_handlers)
