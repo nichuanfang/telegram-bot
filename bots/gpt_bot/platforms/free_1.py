10020f523e2c33364c9c9228f40a00d4433b30c4
f18326186224 <f18326186224@gmail.com>
2024-06-18 01:33:43 +0800

    feat: 新增一个免费GPT

--- bots/gpt_bot/platforms/free_1.py
+++ bots/gpt_bot/platforms/free_1.py
@@ -5,12 +5,11 @@ from bots.gpt_bot.gpt_platform import gpt_platform
 
 
 @gpt_platform
-class Free(Platform):
+class Free_1(Platform):
 	
 	async def query_balance(self):
 		"""
 		查询余额
 		@return: 
 		"""
-		response = await BotHttpRequest.query_balance(self.openai_api_key, self.openai_base_url)
-		return f'已使用 ${round(response["balanceUsed"], 2)} , 订阅总额 ${round(response["balanceTotal"], 2)}'
+		return '已使用 $0.0 , 订阅总额 $0.0'
---
+++ bots/gpt_bot/platforms/free_2.py
@@ -0,0 +1,15 @@
+# 免费的api平台
+from bots.gpt_bot.gpt_http_request import BotHttpRequest
+from bots.gpt_bot.gpt_platform import Platform
+from bots.gpt_bot.gpt_platform import gpt_platform
+
+
+@gpt_platform
+class Free_2(Platform):
+	
+	async def query_balance(self):
+		"""
+		查询余额
+		@return: 
+		"""
+		return '已使用 $0.0 , 订阅总额 $0.0'
