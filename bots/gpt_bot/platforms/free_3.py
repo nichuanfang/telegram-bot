3ee24cacf6b52601811fa55782e407ceee57e629
f18326186224 <f18326186224@gmail.com>
2024-06-25 21:23:16 +0800

    feat: 更新免费模型

--- bots/gpt_bot/platforms/free_3.py
+++ bots/gpt_bot/platforms/free_3.py
@@ -1,37 +1,42 @@
-import re
-import ujson
+# 免费的api平台 gpt-4
+import json
+import js2py
+from fake_useragent import FakeUserAgent
 import requests
-from bots.gpt_bot.gpt_platform import gpt_platform
 from bots.gpt_bot.gpt_platform import Platform
-from fake_useragent import UserAgent
+from bots.gpt_bot.gpt_platform import gpt_platform
 from telegram.ext import CallbackContext
 
+ua = FakeUserAgent(browsers='chrome')
 
-from my_utils.my_logging import get_logger
+token_js = """
+function generateToken(agent) {
+    var d, e, g, f, l, h, k, m, n, p, q, w, r, y, C, I, H, D, t, E, z, N, M, ca, O, P, S, T, J, R, Q, W, X, da, ia, Y, ea, Z, U, aa, fa, ha;
+    p = Math.round(1E11 * Math.random()) + "";
+    q = function() {
+        for (var A = [], F = 0; 64 > F; )
+            A[F] = 0 | 4294967296 * Math.sin(++F % Math.PI);
+        return function(B) {
+            var G, K, L, ba = [G = 1732584193, K = 4023233417, ~G, ~K], V = [], x = unescape(encodeURI(B)) + "\u0080", v = x.length;
+            B = --v / 4 + 2 | 15;
+            for (V[--B] = 8 * v; ~v; )
+                V[v >> 2] |= x.charCodeAt(v) << 8 * v--;
+            for (F = x = 0; F < B; F += 16) {
+                for (v = ba; 64 > x; v = [L = v[3], G + ((L = v[0] + [G & K | ~G & L, L & G | ~L & K, G ^ K ^ L, K ^ (G | ~L)][v = x >> 4] + A[x] + ~~V[F | [x, 5 * x + 1, 3 * x + 5, 7 * x][v] & 15]) << (v = [7, 12, 17, 22, 5, 9, 14, 20, 4, 11, 16, 23, 6, 10, 15, 21][4 * v + x++ % 4]) | L >>> -v), G, K])
+                    G = v[1] | 0,
+                    K = v[2];
+                for (x = 4; x; )
+                    ba[--x] += v[x]
+            }
+            for (B = ""; 32 > x; )
+                B += (ba[x >> 3] >> 4 * (1 ^ x++) & 15).toString(16);
+            return B.split("").reverse().join("")
+        }
+    }();
 
-headers = {
-    'accept': '*/*',
-    'accept-language': 'zh-CN,zh-TW;q=0.9,zh;q=0.8,en;q=0.7,ja;q=0.6',
-    'priority': 'u=1, i',
-    'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
+    return "tryit-" + p + "-" + q(agent + q(agent + q(agent + p + "x")));
 }
-ua = UserAgent()
-logger = get_logger('free_3')
-
-
-def extract_image_url(text):
-    # 正则表达式匹配 ![Image](URL) 格式的字符串
-    pattern = r'\!\[Image\]\((.*?)\)'
-    match = re.search(pattern, text)
-    if match:
-        return match.group(1)  # 返回匹配到的 URL 部分
-    else:
-        raise RuntimeError(text)
+"""
 
 
 @gpt_platform
@@ -44,90 +49,30 @@ class Free_3(Platform):
         """
         return '已使用 $0.0 , 订阅总额 $0.0'
 
-    async def generate_image(self, messages: list):
-        # 生成图片
-        json_data = {
-            'stream': True,
-            'messages': messages,
-            'citations': False,
-            'model': 'gpt-3.5-turbo'
-        }
-        headers.update({
-            'origin': self.foreign_openai_base_url,
-            'user-agent': ua.random,
-            'authorization': self.openai_api_key
-        })
-        with requests.post(
-                f'{self.foreign_openai_base_url}/openai/chat/completions', headers=headers, json=json_data, stream=True) as response:
-            answer = ''
-            for line in response.iter_lines():
-                raw_data = line.decode('utf-8')[6:]
-                if raw_data == '[DONE]':
-                    break
-                if raw_data:
-                    try:
-                        data = ujson.loads(raw_data)
-                    except:
-                        raise RuntimeError('生成图像失败!')
-                    delta = data['choices'][0]['delta']
-                    if 'content' in delta:
-                        answer += delta['content']
-        return extract_image_url(answer)
-
     async def completion(self, stream: bool, context: CallbackContext, *messages, **kwargs):
         new_messages, kwargs = self.chat.combine_messages(
             *messages, **kwargs)
+        new_messages.append({
+            'role': 'user',
+            'content': '用中文回复我!'
+        })
+        payload = {"chas_style": "how-ai",
+                   "chatHistory": json.dumps(new_messages)}
+        agent = ua.random
+        generateToken = js2py.eval_js(token_js)
+        token = generateToken(agent)
+        # api_key = js2py.eval_js(token_js.replace('agent', agent))
+        headers = {
+            "api-key": token,
+            "User-Agent": agent,
+        }
+        response = requests.post("https://api.deepai.org/hacking_is_a_serious_crime",
+                                 headers=headers, data=payload, stream=True)
         answer = ''
-        if stream:
-            json_data = {
-                'stream': True,
-                'messages': new_messages,
-                'citations': False,
-                **kwargs
-            }
-            headers.update({
-                'origin': self.foreign_openai_base_url,
-                'user-agent': ua.random,
-                'authorization': self.openai_api_key
-            })
-            with requests.post(
-                    f'{self.foreign_openai_base_url}/openai/chat/completions', headers=headers, json=json_data, stream=True) as response:
-                for line in response.iter_lines():
-                    raw_data = line.decode('utf-8')[6:]
-                    if raw_data == '[DONE]':
-                        yield 'finished', answer
-                        break
-                    if raw_data:
-                        try:
-                            data = ujson.loads(raw_data)
-                        except:
-                            raise RuntimeError(raw_data)
-                        delta = data['choices'][0]['delta']
-                        if 'content' in delta:
-                            answer += delta['content']
-                        yield 'not_finished', answer
-
-        else:
-            # todo 此平台的图片解析有问题 需要研究
-            json_data = {
-                'stream': False,
-                'messages': new_messages,
-                'citations': False,
-                **kwargs
-            }
-            headers.update({
-                'origin': self.foreign_openai_base_url,
-                'user-agent': ua.random,
-                'content-type': 'application/json',
-                'authorization': self.openai_api_key
-            })
-            completion = requests.post(
-                f'{self.foreign_openai_base_url}/openai/chat/completions', headers=headers, json=json_data)
-            try:
-                answer = ujson.loads(completion.text)[
-                    'choices'][0]['message']['content']
-                yield answer
-            except:
-                raise RuntimeError(completion.text)
+        for chunk in response.iter_content(chunk_size=None):
+            response.raise_for_status()
+            answer += chunk.decode()
+            yield 'not_finished', answer
+        yield 'finished', answer
         await self.chat.append_messages(
             answer, context, *messages)
