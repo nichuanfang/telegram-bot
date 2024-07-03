32d764311ced28505ba6c0347a8cfe55942779b4
f18326186224 <f18326186224@gmail.com>
2024-07-03 20:51:15 +0800

    feat: 自定义解析器 使用smartdns

--- my_utils/global_var.py
+++ my_utils/global_var.py
@@ -17,13 +17,14 @@ if sys.platform == 'win32':
 class CustomResolver(aiohttp.abc.AbstractResolver):
     """ 自定义dns解析器 """
 
-    def __init__(self, dns_map: Dict[str, Tuple[str, int]], default_dns: str, default_family=socket.AF_INET):
-        self.dns_map = dns_map
-        self.default_dns = default_dns
-        self.default_family = default_family
-        self.resolver = aiodns.DNSResolver(nameservers=[default_dns])
+    def __init__(self, used_dns: str):
+        self.resolver = aiodns.DNSResolver(nameservers=[used_dns])
 
-    def generate_result(self, host, answers, port, family):
+    async def _query(self, host: str, family: int, port: int) -> list:
+        """ 进行 DNS 查询 """
+
+        # 只查询A记录
+        result = await self.resolver.query(host, 'A')
         return [
             {
                 'hostname': host,
@@ -33,52 +34,11 @@ class CustomResolver(aiohttp.abc.AbstractResolver):
                 'proto': 0,
                 'flags': 0,
             }
-            for answer in answers
+            for answer in result
         ]
-
-    def find_dns_config(self, host: str) -> Tuple[str, int]:
-        """ 查找完整匹配或泛域名匹配的 DNS 配置 """
-        parts = host.split('.')
-        for i in range(len(parts)):
-            domain = '.'.join(parts[i:])
-            if domain in self.dns_map:
-                return self.dns_map[domain]
-        return self.default_dns, self.default_family
-
-    async def _query(self, host: str, family: int, port: int) -> list:
-        """ 进行 DNS 查询 """
-        ipv4_res, ipv6_res = await asyncio.gather(
-            self.resolver.query(host, 'A'),
-            self.resolver.query(host, 'AAAA'),
-            return_exceptions=True
-        )
-        if family == socket.AF_INET:
-            if ipv4_res and not isinstance(ipv4_res, BaseException):
-                return self.generate_result(host, ipv4_res, port, socket.AF_INET)
-            elif ipv6_res and not isinstance(ipv6_res, BaseException):
-                return self.generate_result(host, ipv6_res, port, socket.AF_INET6)
-            else:
-                return [{
-                    'hostname': host,
-                    'host': None,
-                    'port': port,
-                    'family': family,
-                    'proto': 0,
-                    'flags': 0,
-                }]
-        else:
-            if ipv6_res and not isinstance(ipv6_res, BaseException):
-                return self.generate_result(host, ipv6_res, port, socket.AF_INET6)
-            elif ipv4_res and not isinstance(ipv4_res, BaseException):
-                return self.generate_result(host, ipv4_res, port, socket.AF_INET)
-            else:
-                return []
 
     async def resolve(self, host: str, port=0, family=socket.AF_UNSPEC) -> list:
         """ 解析给定主机名的地址 """
-        dns, priority_family = self.find_dns_config(host)
-        family = priority_family if family == socket.AF_UNSPEC else family
-        self.resolver.nameservers = [dns]
         return await self._query(host, family, port)
 
     async def close(self):
@@ -86,14 +46,8 @@ class CustomResolver(aiohttp.abc.AbstractResolver):
         pass
 
 
-dns_map = {
-    'dogyun.com': ('223.5.5.5', socket.AF_INET),
-    'ffa.chat': ('223.5.5.5', socket.AF_INET)
-}
-default_dns = '1.1.1.1'
-default_family = socket.AF_INET
 # 创建自定义解析器实例
-custom_resolver = CustomResolver(dns_map, default_dns, default_family)
+custom_resolver = CustomResolver('127.0.0.1')
 
 # 全局会话
 GLOBAL_SESSION = aiohttp.ClientSession(
